d6c0a04fd687638cd89ada0b55426d741eda3598
mufradhossain <79082294+mufradhossain@users.noreply.github.com>
2021-12-02 23:54:12 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,25 +7,29 @@ conn = sqlite3.connect('diucse.db')
 def teamview(id):
 
     cur= conn.cursor()
-    cur.execute("select Team_Name,Leader_Name,Leader_Id,Leader_Image,Member_Name,Member_Id,Member_Image,Title,Supervisor,`Co-Supervisor` from defense where Leader_Id = '"+str(id)+"'")
+    cur.execute("select Team_Name,Leader_Name,Leader_Id,Leader_Image,Member_Name,Member_Id,Member_Image,Title,Supervisor,`Co-Supervisor`,Description,Objective,Motivation from defense where Leader_Id = '"+str(id)+"'")
     hol=cur.fetchone()
-    st.header(hol[0])
-    col1, col2 = st.columns(2)
-    with col1:
-        st.image(hol[3],width=200)
-        st.write(hol[1]+" (Leader)")
-        st.write("ID: "+hol[2])
-    with col2:
-        try:
-            st.image(hol[6],width=200)
-            st.write(hol[4])
-            st.write("ID: "+hol[5])
-        except:
-            pass
-
-    st.subheader("Title: "+hol[7])
-    st.write("Supervisor: "+hol[8])
-    st.write("Co-Supervisor: "+hol[9])
+    #st.header(hol[0])
+    #
+    with st.expander(hol[0],expanded=False):
+        st.subheader("Title: "+hol[7])
+        col1, col2 = st.columns(2)
+        with col1:
+            st.image(hol[3],width=200)
+            st.write(hol[1]+" (Leader)")
+            st.write("ID: "+hol[2])
+        with col2:
+            try:
+                st.image(hol[6],width=200)
+                st.write(hol[4])
+                st.write("ID: "+hol[5])
+            except:
+                pass
+        st.info("Supervisor: "+hol[8])
+        st.info("Co-Supervisor: "+hol[9])
+        st.info("Description: "+hol[10])
+        st.info("Objective: "+hol[11])
+        st.info("Motivation: "+hol[12])
     return
 
 
