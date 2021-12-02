6f25893e1f71c7af0ddcb4a1bfbb22c326c88693
mufradhossain <79082294+mufradhossain@users.noreply.github.com>
2021-12-02 18:23:51 +0600

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import mysql.connector
+
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+
+def teamview(id):
+
+    cur= conn.cursor()
+    cur.execute("select Team_Name,Leader_Name,Leader_Id,Leader_Image,Member_Name,Member_Id,Member_Image,Title,Supervisor,`Co-Supervisor` from defense where Leader_Id = '"+str(id)+"'")
+    hol=cur.fetchone()
+    st.header(hol[0])
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write(hol[1]+" (Leader)")
+        st.image(hol[3],width=150)
+        st.write("ID: "+hol[2])
+    with col2:
+        st.write(hol[4])
+        st.image(hol[6],width=150)
+        st.write("ID: "+hol[5])
+    st.subheader("Title: "+hol[7])
+    st.write("Supervisor: "+hol[8])
+    st.write("Co-Supervisor: "+hol[9])
+    return
+
+
+m = range(2341,2774)
+for n in m:
+    try:
+        teamview("191-15-"+str(n))
+    except:
+        pass
